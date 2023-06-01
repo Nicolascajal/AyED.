b8006b5f5018142f53f384623ef638b1a0e0d8ef
Nicolascajal <134395819+Nicolascajal@users.noreply.github.com>
2023-06-01 18:27:40 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+
+    float c = 0.0;
+    int i = 0;
+    
+    while(abs(c*4 - M_PI) >= 0.000001){
+        float termino = pow(-1, i) / ((2 * i) + 1);
+        c += termino;
+        i++;
+    }
+
+    cout.precision(7);
+
+    cout << "Valor aproximado de pi: " << c*4 << endl;
+
+    return 0;
+}
 
